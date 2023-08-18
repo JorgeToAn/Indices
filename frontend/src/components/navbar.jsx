5e33214b13e9fab2ab438d20d787f29af0292d7b
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-08-18 11:17:28 -0700

    Se agrego css a la navbar para utilizar de mejor manera el espacio

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,3 +1,4 @@
+import "./navbar.css";
 import { ChevronDown, Home, Search, UserCircle } from "tabler-icons-react";
 import {
     Header, 
@@ -12,92 +13,97 @@ import {
 const NavBar = () => {
     return (
         <Header bg="negro" height={40}>
-            <Group>
-                <Button color="negro" leftIcon={<Home />} uppercase={true}>
-                    Inicio
-                </Button>
+            <div className="nav">
+                <Group>
+                    <Button color="negro" leftIcon={<Home />} uppercase={true}>
+                        Inicio
+                    </Button>
+                    <TextInput placeholder="BUSCAR" icon={<Search />} size="xs"/>
 
-                <TextInput placeholder="BUSCAR" icon={<Search />} size="xs"/>
+                </Group>
 
-                {/* Menu de tablas */}
-                <Menu trigger="hover" openDelay={100} closeDelay={400}>
-                    <Menu.Target>
-                        <Button color="negro" uppercase={true}>
-                            Tablas
-                            <ChevronDown size={16} strokeWidth={2} color={'white'} />
-                        </Button>
-                    </Menu.Target>
+                <Group>
+                    {/* Menu de tablas */}
+                    <Menu trigger="hover" openDelay={100} closeDelay={400}>
+                        <Menu.Target>
+                            <Button color="negro" uppercase={true}>
+                                Tablas
+                                <ChevronDown size={16} strokeWidth={2} color={'white'} />
+                            </Button>
+                        </Menu.Target>
 
-                    <Menu.Dropdown>
-                        <Menu.Item>POBLACION</Menu.Item>
-                        <Menu.Item >CRECIMIENTO</Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
+                        <Menu.Dropdown>
+                            <Menu.Item>POBLACION</Menu.Item>
+                            <Menu.Item >CRECIMIENTO</Menu.Item>
+                        </Menu.Dropdown>
+                    </Menu>
 
-                {/* Menu de indices */}
-                <Menu trigger="hover" openDelay={100} closeDelay={400}>
-                    <Menu.Target>
-                        <Button color="negro">
-                            INDICES
-                            <ChevronDown size={16} strokeWidth={2} color={'white'} />
-                        </Button>
-                    </Menu.Target>
+                    {/* Menu de indices */}
+                    <Menu trigger="hover" openDelay={100} closeDelay={400}>
+                        <Menu.Target>
+                            <Button color="negro">
+                                INDICES
+                                <ChevronDown size={16} strokeWidth={2} color={'white'} />
+                            </Button>
+                        </Menu.Target>
 
-                    <Menu.Dropdown>
-                        <Menu.Item>PERMANENCIA</Menu.Item>
-                        <Menu.Item >EGRESO</Menu.Item>
-                        <Menu.Item >TITULACION</Menu.Item>
-                        <Menu.Item >DESERCION</Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
+                        <Menu.Dropdown>
+                            <Menu.Item>PERMANENCIA</Menu.Item>
+                            <Menu.Item >EGRESO</Menu.Item>
+                            <Menu.Item >TITULACION</Menu.Item>
+                            <Menu.Item >DESERCION</Menu.Item>
+                        </Menu.Dropdown>
+                    </Menu>
 
-                {/* Menu de reportes */}
-                <Menu trigger="hover" openDelay={100} closeDelay={400}>
-                    <Menu.Target>
-                        <Button color="negro">
-                            REPORTES
-                            <ChevronDown size={16} strokeWidth={2} color={'white'} />
-                        </Button>
-                    </Menu.Target>
+                    {/* Menu de reportes */}
+                    <Menu trigger="hover" openDelay={100} closeDelay={400}>
+                        <Menu.Target>
+                            <Button color="negro">
+                                REPORTES
+                                <ChevronDown size={16} strokeWidth={2} color={'white'} />
+                            </Button>
+                        </Menu.Target>
 
-                    <Menu.Dropdown>
-                        <Menu.Item>NUEVO INGRESO</Menu.Item>
-                        <Menu.Item >EGRESADOS</Menu.Item>
-                        <Menu.Item >TITULADOS</Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
+                        <Menu.Dropdown>
+                            <Menu.Item>NUEVO INGRESO</Menu.Item>
+                            <Menu.Item >EGRESADOS</Menu.Item>
+                            <Menu.Item >TITULADOS</Menu.Item>
+                        </Menu.Dropdown>
+                    </Menu>
 
-                {/* Menu de cedulas */}
-                <Menu trigger="hover" openDelay={100} closeDelay={400}>
-                    <Menu.Target>
-                        <Button color="negro">
-                            CEDULAS
-                            <ChevronDown size={16} strokeWidth={2} color={'white'} />
-                        </Button>
-                    </Menu.Target>
+                    {/* Menu de cedulas */}
+                    <Menu trigger="hover" openDelay={100} closeDelay={400}>
+                        <Menu.Target>
+                            <Button color="negro">
+                                CEDULAS
+                                <ChevronDown size={16} strokeWidth={2} color={'white'} />
+                            </Button>
+                        </Menu.Target>
 
-                    <Menu.Dropdown>
-                        <Menu.Item>CACECA</Menu.Item>
-                        <Menu.Item >CACEI</Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
+                        <Menu.Dropdown>
+                            <Menu.Item>CACECA</Menu.Item>
+                            <Menu.Item >CACEI</Menu.Item>
+                        </Menu.Dropdown>
+                    </Menu>
 
-                {/* Menu de alumnos */}
-                <Menu trigger="hover" openDelay={100} closeDelay={400}>
-                    <Menu.Target>
-                        <Button color="negro">
-                            ALUMNOS
-                            <ChevronDown size={16} strokeWidth={2} color={'white'} />
-                        </Button>
-                    </Menu.Target>
+                    {/* Menu de alumnos */}
+                    <Menu trigger="hover" openDelay={100} closeDelay={400}>
+                        <Menu.Target>
+                            <Button color="negro">
+                                ALUMNOS
+                                <ChevronDown size={16} strokeWidth={2} color={'white'} />
+                            </Button>
+                        </Menu.Target>
 
-                    <Menu.Dropdown>
-                        <Menu.Item>HISTORIAL POR ALUMNO</Menu.Item>
-                        <Menu.Item >LISTA DE ALUMNOS</Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
+                        <Menu.Dropdown>
+                            <Menu.Item>HISTORIAL POR ALUMNO</Menu.Item>
+                            <Menu.Item >LISTA DE ALUMNOS</Menu.Item>
+                        </Menu.Dropdown>
+                    </Menu>
+                </Group>
+                
 
-                {/* Menu de alumnos */}
+                {/* Menu de usuario */}
                 <Menu trigger="hover" openDelay={100} closeDelay={400}>
                     <Menu.Target>
                         <Button color="negro" leftIcon={<UserCircle />}>
@@ -110,7 +116,7 @@ const NavBar = () => {
                         <Menu.Item >CERRAR SESION</Menu.Item>
                     </Menu.Dropdown>
                 </Menu>
-            </Group>
+            </div>
         </Header>
     );
 }
