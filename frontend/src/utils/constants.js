a07f7e300eed0205c7e18897ec54189231651bc2
JorgeToAn <jorgetosta.f@gmail.com>
2023-09-11 20:24:15 -0700

    Agregar constantes al front

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,17 @@
 export const API_BASE_URL = 'http://localhost:8000/';
 
-// SEXO CHOICES
-export const SEXO_HOMBRE = 0;
-export const SEXO_MUJER = 1;
-export const SEXO_INDEFINIDO = 2;
+// GENEROS
+export const GENERO_HOMBRE = 0;
+export const GENERO_MUJER = 1;
+export const GENERO_OTRO = 2;
+
+// TIPOS INGRESOS
+export const EXAMEN = 'EX';
+export const EQUIVALENCIA = 'EQ';
+export const TRASLADO = 'TR';
+export const CONVALIDACION = 'CO';
+
+// TIPOS TITULACIONES
+export const TESIS = 'TE';
+export const RESIDENCIA = 'RE';
+export const DUAL = 'DU';
