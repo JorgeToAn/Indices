6109bbf64aef0cecd91a32afe044d6fe95da540c
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-09-20 22:39:57 -0700

    Primeras pruebas de la funcion de manejo de tablas de permanencia

--- frontend/src/mockup/dataService.js
+++ frontend/src/mockup/dataService.js
@@ -16,35 +16,55 @@ const tablaPermanencia = [
     ['Semestre 13', '2020-2', '6','2','2','2','4.88'],
     ['Semestre 14', '2021-1', '2','2','0','0','4.88'],
     ['Semestre 15', '2021-2', '2','2','0','0','4.88'],
-]
+];
 
-function datosIndicesPermanencia(cohorte, numSemestres, categoria, carrera) {
+function datosIndicesPermanencia(cohorte, numSemestres, carrera) {
     const tabla = [];
-    const activoInd = 0;
-    const desercionInd = 0;
-    const matriculaInd = 0;
-    const tasaInd = 0;
+    let activoInd = 0;
+    let desercionInd = 0;
+    let matriculaInd = 0;
+    let tasaInd = 0;
+    let periodo = [];
 
-    tabla.append(tablaPermanencia[0]);
+    tabla.push(tablaPermanencia[0]);
     tabla[0][0] += cohorte + " " + carrera;
-    tabla.append(tablaPermanencia[1]);
+    tabla.push(tablaPermanencia[1]);
     for (const c in tabla[1]) {
         switch(tabla[1][c]) {
-            case 'inscritos': 
+            case 'inscritos':
                 activoInd = c;
                 break;
-            case 'desercion': 
+            case 'desercion':
                 desercionInd = c;
                 break;
-            case 'matricula final': 
+            case 'matricula final':
                 matriculaInd = c;
                 break;
-            case 'tasa de retencion': 
+            case 'tasa de retencion':
                 tasaInd = c;
                 break;
             default: continue;
         }
     }
+    for (let sem = 0; sem < numSemestres; sem++) {
+        if (sem === 0){
+            periodo = cohorte.split("-");
+        } else {
+            periodo = anioPeriodo(periodo);
+        }
+        tabla[sem][1] = periodo[0]+"-"+periodo[1];
+        tabla[sem][matriculaInd] = tabla[sem][activoInd]-tabla[sem][desercionInd];
+        tabla[sem][tasaInd] = (tabla[sem][matriculaInd]/tabla[sem][activoInd])*100;
+    }
+    return tabla;
 };
 
+function anioPeriodo(periodoAnterior) {
+    if (periodoAnterior[0] === '2') {
+        periodoAnterior[0] = Number(periodoAnterior[0]) + 1;
+    } else if (periodoAnterior.periodo === '1') {
+        periodoAnterior[1] = Number(periodoAnterior[1])+ 1;
+    }
+    return periodoAnterior;
+}
 export default  datosIndicesPermanencia;
--- frontend/src/views/indices/Permanencia.jsx
+++ frontend/src/views/indices/Permanencia.jsx
@@ -1,31 +1,45 @@
-import { Checkbox, Flex, Group } from '@mantine/core';
+import { Button, Checkbox, Flex, Group } from '@mantine/core';
 import Header from './../../components/header';
 import Tabla from './../../components/Tabla';
 import Dropdown from './../../components/Dropdown';
+import datosIndicesPermanencia from '../../mockup/dataService';
 
 const IndicePermanencia = () => {
     // Informacion de prueba, no representa el comportamiento real
-    const tabla = [
-        ['Semestre 1', '2015-1', '41','0','10','31','75.61'],
-        ['Semestre 2', '2015-2', '31','0','5','26','63.41'],
-        ['Semestre 4', '2016-1', '26','0','3','23','56.10'],
-        ['Semestre 5', '2016-2', '23','0','2','21','51.22'],
-        ['Semestre 6', '2017-1', '21','0','0','21','51.22'],
-        ['Semestre 7', '2017-2', '18','0','0','18','43.90'],
-        ['Semestre 8', '2018-1', '16','0','0','16','39.02'],
-        ['Semestre 9', '2018-2', '16','0','0','16','39.02'],
-        ['Semestre 10', '2019-1', '16','1','2','16','39.02'],
-        ['Semestre 11', '2019-2', '13','5','0','8','19.51'],
-        ['Semestre 12', '2020-1', '8','2','0','6','14.63'],
-        ['Semestre 13', '2020-2', '6','2','2','2','4.88'],
-        ['Semestre 14', '2021-1', '2','2','0','0','4.88'],
-        ['Semestre 15', '2021-2', '2','2','0','0','4.88'],
-    ];
+    // const tabla = [
+    //     ['Semestre 1', '2015-1', '41','0','10','31','75.61'],
+    //     ['Semestre 2', '2015-2', '31','0','5','26','63.41'],
+    //     ['Semestre 4', '2016-1', '26','0','3','23','56.10'],
+    //     ['Semestre 5', '2016-2', '23','0','2','21','51.22'],
+    //     ['Semestre 6', '2017-1', '21','0','0','21','51.22'],
+    //     ['Semestre 7', '2017-2', '18','0','0','18','43.90'],
+    //     ['Semestre 8', '2018-1', '16','0','0','16','39.02'],
+    //     ['Semestre 9', '2018-2', '16','0','0','16','39.02'],
+    //     ['Semestre 10', '2019-1', '16','1','2','16','39.02'],
+    //     ['Semestre 11', '2019-2', '13','5','0','8','19.51'],
+    //     ['Semestre 12', '2020-1', '8','2','0','6','14.63'],
+    //     ['Semestre 13', '2020-2', '6','2','2','2','4.88'],
+    //     ['Semestre 14', '2021-1', '2','2','0','0','4.88'],
+    //     ['Semestre 15', '2021-2', '2','2','0','0','4.88'],
+    // ];
 
-    const headers = [
-        ['Indices de rendimiento escolar cohorte generacional 2015-1 ingenieria mecanica'],
-        ['Semestre', 'Periodo', 'Inscritos', 'Egresados','Desercion', 'Matricula final','Tasa de retencion'],
-     ];
+    // const headers = [
+    //     ['Indices de rendimiento escolar cohorte generacional 2015-1 ingenieria mecanica'],
+    //     ['Semestre', 'Periodo', 'Inscritos', 'Egresados','Desercion', 'Matricula final','Tasa de retencion'],
+    //  ];
+    const tabla = [];
+    const headers = [];
+    let tablaCompleta = [];
+    const handleTable = () => {
+        tablaCompleta = datosIndicesPermanencia("2016-1", 9, "Ingenieria en Sistemas Computacionales");
+        headers.push(tablaCompleta[0]);
+        headers.push(tablaCompleta[1]);
+
+        for (let fila = 2; fila < tablaCompleta.length; fila++) {
+            tabla.push(tablaCompleta[fila]);
+        }
+    };
+
     return(
         <div style={{
             width: '100vw',
@@ -58,6 +72,7 @@ const IndicePermanencia = () => {
                         ['Excel','Excel'],
                         ['PDF','PDF'],
                     ]} />
+                    <Button onClick={handleTable}>Activar</Button>
                 </Group>
                 <Group mt={0} mb={16} >
                     <Checkbox labelPosition='left' label='Examen y Convalidación' radius='sm' />
