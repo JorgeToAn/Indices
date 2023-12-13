70942a4cafc162d2d1761901a1ff57c600101ae6
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-12-12 18:24:05 -0800

    Se agregaron nuevos estilos

--- frontend/src/views/cedulas/Cacei.jsx
+++ frontend/src/views/cedulas/Cacei.jsx
@@ -3,17 +3,18 @@ import Header from './../../components/header';
 import Tabla from './../../components/Tabla';
 import Dropdown from './../../components/Dropdown';
 import {  useEffect, useState } from 'react';
-// import { useInputState } from '@mantine/hooks';
-// import dropDownData from '../../mockup/dropDownData';
-// import "./Indices.css";
+import { useInputState } from '@mantine/hooks';
+import dropDownData from '../../mockup/dropDownData';
+import "../indices/Indices.css";
+// import dataService from '../../mockup/dataService';
 
 const CedulaCacei = () => {
     // Heading y data almacenan la informacion de los encabezados y el contenido de la tabla, respectivamente
     const [heading, setHeading] = useState([]);
     const [data, setData] = useState([]);
     // Cohorte, carrera y numSemestres son los datos de los Select
-    // const [cohorte, setCohorte] = useInputState('');
-    // const [carrera, setCarrera] = useInputState('');
+    const [cohorte, setCohorte] = useInputState('');
+    const [carrera, setCarrera] = useInputState('');
 
     // const handleTable = () => {
     //     const tabla = [];
@@ -30,12 +31,12 @@ const CedulaCacei = () => {
     //     setData(tabla);
     // };
 
-    // const checkFilters = () => {
-    //     if (cohorte === "" || carrera === "") {
-    //         return true;
-    //     }
-    //     return false;
-    // };
+    const checkFilters = () => {
+        if (cohorte === "" || carrera === "") {
+            return true;
+        }
+        return false;
+    };
 
     useEffect(() => {
         const header = [
@@ -57,8 +58,8 @@ const CedulaCacei = () => {
                 <fieldset className='filtros'>
                     <legend>Filtros</legend>
                     <Group mt={0} mb={16} color='gris'>
-                        {/* <Dropdown  label="Programa educativo" color="#FF785A" handleChangeFn={setCarrera}  />
-                        <Dropdown  label="Cohorte generacional" color="#FF785A" handleChangeFn={setCohorte} /> */}
+                        <Dropdown  label="Programa educativo" color="#FF785A" handleChangeFn={setCarrera}  data={dropDownData.carreras}/>
+                        <Dropdown  label="Cohorte generacional" color="#FF785A" handleChangeFn={setCohorte} data={dropDownData.cohortes}/>
                         <Dropdown  label="Exportar" color="#FF785A" data={[
                             ['Excel','Excel'],
                             ['PDF','PDF'],
@@ -69,7 +70,7 @@ const CedulaCacei = () => {
                         <Checkbox labelPosition='left' label='Traslado y Equivalencia' radius='sm' />
                     </Group>
                     <Group style={{ justifyContent: "flex-end" }} >
-                        <Button  color='negro'>Filtrar</Button>
+                        <Button disabled={checkFilters} color='negro'>Filtrar</Button>
                     </Group>
                 </fieldset>
                 <Tabla colors="tabla-toronja" headers={heading} content={data} />
