9607e982ffc91bb7ceae09b9bbefb0251b8d3034
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-12-14 12:59:35 -0800

    Se agrego funcion de  habilitar y deshabilitar boton de cambio

--- frontend/src/views/usuario/CambioContrasena.jsx
+++ frontend/src/views/usuario/CambioContrasena.jsx
@@ -1,23 +1,35 @@
-import { Button, Flex, Group, Text, TextInput } from "@mantine/core";
+import { Button, Center, Flex, Group, Text, TextInput } from "@mantine/core";
 import Header from "../../components/header";
+import { useInputState } from "@mantine/hooks";
 
 const CambioContrasena = () => {
+    // Bloquear boton hasta que los 4 campos sean correctos
+    const [password, setPassword] = useInputState('');
+    const [newPassword, setNewPassword] = useInputState('');
+    const [copyNewPassword, setCopyNewPassword] = useInputState('');
+
+
+    const handleChange = () => {
+        return password && (newPassword === copyNewPassword) && newPassword && copyNewPassword ? true :  false;
+    };
     return(
         <div style={{
             align: "left",
             padding: "2vw"
         }}>
             <Header color="naranja" route="/" section="Usuario" title="Cambio de contraseña" />
-            <Group align="flex-start" mt={20} spacing="xl" >
-                <Flex direction="column">
+            <Group  align="center" justify="center" mt={20} spacing="xl" >
+                <Flex direction="column" align="center">
+                <Text>En seguida podrás cambiar tu contraseña actual del sistema, recuerda que debe tener 8 caracteres como mínimo, 1 número, 1 caracter especial, 1 letra minúscula y 1 letra mayúscula.</Text>
                     <form>
-                        <Text>En seguida podrás cambiar tu contraseña actual del sistema, recuerda que debe tener 8 caracteres como mínimo, 1 número, 1 caracter especial, 1 letra minúscula y 1 letra mayúscula.</Text>
-                        <Group w='25vw'>
-                            <TextInput id="password" w="100%" color="naranja" label="Contraseña actual" withAsterisk/>
-                            <TextInput id="newPassword" w="100%" label="Contraseña nueva" withAsterisk />
-                            <TextInput id="copyNewPassword" w="100%" label="Repita la contraseña nueva" withAsterisk />
+                        <Group w='25vw' align="center" justify="center">
+                            <TextInput id="password" value={password} onChange={setPassword} w="100%" color="naranja" label="Contraseña actual" withAsterisk/>
+                            <TextInput id="newPassword" value={newPassword} onChange={setNewPassword} w="100%" label="Contraseña nueva" withAsterisk />
+                            <TextInput id="copyNewPassword" value={copyNewPassword} onChange={setCopyNewPassword} w="100%" label="Repita la contraseña nueva" withAsterisk />
                         </Group>
-                        <Button mt={10} color="naranja">Cambiar</Button>
+                        <Center>
+                            <Button mt={10} color="naranja" disabled={!handleChange()}>Cambiar</Button>
+                        </Center>
                     </form>
                 </Flex>
             </Group>
