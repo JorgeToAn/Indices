b53783ae91842ca6b88451a4eaf65e8a96ff898c
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-12-13 16:31:10 -0800

    Se agrego la habilitacion y deshabilitacion de inputs en formulario de historial de alumnos

--- frontend/src/views/alumnos/AlumnosHistorial.jsx
+++ frontend/src/views/alumnos/AlumnosHistorial.jsx
@@ -30,17 +30,18 @@ const AlumnosHistorial = () => {
                 <Flex direction="column">
                     <form>
                         <TextInput label="Buscar"  icon={<Search width={20} />} />
-                        <TextInput label="Nombre" withAsterisk/>
-                        <TextInput label="No. de control" withAsterisk/>
+                        <TextInput label="Nombre" disabled={!editar} withAsterisk/>
+                        <TextInput label="No. de control" disabled={!editar} withAsterisk/>
                         <Group className="input-group">
-                            <TextInput label="Sexo" withAsterisk width="45%"/>
-                            <TextInput label="Edad" withAsterisk width="45%"/>
+                            <TextInput label="Sexo" disabled={!editar} withAsterisk width="45%"/>
+                            <TextInput label="Edad" disabled={!editar} withAsterisk width="45%"/>
                         </Group>
                         <Group className="input-group">
                             <Select
                                 width="45%"
                                 label="Estatus"
-                                placeholder="Inscrito"
+                                disabled={!editar}
+                                placeholder="Seleccionar un estatus"
                                 data={[
                                     {value: 'Inscrito', label: 'Inscrito'},
                                     {value: 'Baja', label: 'Baja'},
@@ -52,12 +53,13 @@ const AlumnosHistorial = () => {
                             <Select
                                 width="45%"
                                 label="Semestre"
+                                disabled={!editar}
                                 placeholder="Seleccione un semestre"
                                 data={dropDownData.semestres.map((fila) => ({"value":fila[0], "label":fila[1]}))}
                                 withAsterisk
                             />
                         </Group>
-                        <Checkbox labelPosition='left' color='toronja' mt={15} label='Liberacion de inglés (2018-1)' radius='sm' />
+                        <Checkbox labelPosition='left' color='toronja' disabled={!editar} mt={15} label='Liberacion de inglés (2018-1)' radius='sm' />
                         { editar ?
                             <Group className="input-group">
                                 <Button type="button" mt={16} leftIcon={<DeviceFloppy />} >Guardar</Button>
