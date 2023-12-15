c4443007cc9e2bedd230946ebea6fc4265d397de
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-12-14 20:03:38 -0800

    Avance

--- frontend/src/views/usuario/CambioContrasena.jsx
+++ frontend/src/views/usuario/CambioContrasena.jsx
@@ -1,8 +1,19 @@
-import { Button, Center, Flex, Group, Text, TextInput } from "@mantine/core";
+import { Button, Center, Flex, Group, Text, TextInput, createStyles } from "@mantine/core";
 import Header from "../../components/header";
 import { useInputState } from "@mantine/hooks";
 
+const useStyles = createStyles((theme) => ({
+    orangeInput: {
+        '&:focus-within':{
+            borderColor: '#FFAA5A',
+        }
+    },
+    orangeAsterisk: {
+        color: '#FFAA5A',
+    }
+}));
 const CambioContrasena = () => {
+    const { classes} = useStyles();
     // Bloquear boton hasta que los 4 campos sean correctos
     const [password, setPassword] = useInputState('');
     const [newPassword, setNewPassword] = useInputState('');
@@ -23,12 +34,12 @@ const CambioContrasena = () => {
                 <Text>En seguida podrás cambiar tu contraseña actual del sistema, recuerda que debe tener 8 caracteres como mínimo, 1 número, 1 caracter especial, 1 letra minúscula y 1 letra mayúscula.</Text>
                     <form>
                         <Group w='25vw' align="center" justify="center">
-                            <TextInput id="password" value={password} onChange={setPassword} w="100%" color="naranja" label="Contraseña actual" withAsterisk/>
-                            <TextInput id="newPassword" value={newPassword} onChange={setNewPassword} w="100%" label="Contraseña nueva" withAsterisk />
-                            <TextInput id="copyNewPassword" value={copyNewPassword} onChange={setCopyNewPassword} w="100%" label="Repita la contraseña nueva" withAsterisk />
+                            <TextInput id="password" classNames={{ input: classes.orangeInput, required: classes.orangeAsterisk}} value={password} onChange={setPassword} w="100%" color="naranja" label="Contraseña actual" withAsterisk/>
+                            <TextInput id="newPassword" classNames={{ input: classes.orangeInput, required: classes.orangeAsterisk}} value={newPassword} onChange={setNewPassword} w="100%" label="Contraseña nueva" withAsterisk />
+                            <TextInput id="copyNewPassword" classNames={{ input: classes.orangeInput, required: classes.orangeAsterisk}} value={copyNewPassword} onChange={setCopyNewPassword} w="100%" label="Repita la contraseña nueva" withAsterisk />
                         </Group>
                         <Center>
-                            <Button mt={10} color="naranja" disabled={!handleChange()}>Cambiar</Button>
+                            <Button mt={10} color="naranja" disabled={!handleChange()}>Cambiar contraseña</Button>
                         </Center>
                     </form>
                 </Flex>
