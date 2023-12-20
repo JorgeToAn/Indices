d7dc188d3d972ea2e290aecf684d5f89449d4b39
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-12-20 11:34:28 -0800

    Se mejoro el estilo del multi select

--- frontend/src/views/ModalEditarUsuario.jsx
+++ frontend/src/views/ModalEditarUsuario.jsx
@@ -9,29 +9,61 @@ import {
 } from "@mantine/core";
 import { DeviceFloppy } from "tabler-icons-react";
 import { PropTypes } from 'prop-types';
+import "./ModalEditarUsuario.css";
 
 
 function ModalEditarUsuario ({opened, close, info}) {
 
     return (
-        <Modal.Root opened={opened} onClose={close} centered closeOnClickOutside={false}>
+        <Modal.Root opened={opened} onClose={close} centered size="lg" closeOnClickOutside={false}>
             <Modal.Overlay />
             <Modal.Content>
                 <Modal.Header>
-                    <Modal.Title  fw="bold">Actualizando Usuario</Modal.Title>
+                    <Modal.Title  fw="bold" ta="center" ml="auto">Actualizando Usuario</Modal.Title>
                     <Modal.CloseButton bg="gris" color="negro"></Modal.CloseButton>
                 </Modal.Header>
                 <Modal.Body>
-                    <Flex direction="row">
-                        <Flex direction="column">
-                            <Text>Datos de usuario</Text>
+                    <Flex direction="row" align="flex-start" justify="center" gap={20}>
+                        <Flex direction="column" className="datos" w="45%">
+                            <Text fw="bold">Datos de usuario</Text>
                             <TextInput label="Id de usuario" disabled value={info[0]}/>
                             <TextInput label="Nombre de usuario" disabled value={info[1]}/>
                             <TextInput label="Correo electrónico" disabled value={info[2]}/>
                         </Flex>
-                        <Flex direction="column">
-                            <Text>Permisos</Text>
-                            <MultiSelect label="Tablas" data={['Consultar', 'Exportar']}/>
+                        <Flex direction="column" className="datos" w="45%">
+                            <Text fw="bold">Permisos</Text>
+                            <MultiSelect label="Tablas" data={['Consultar <i class="fa-solid fa-eye"></i>', 'Exportar']} styles={(theme)=>({
+                                wrapper: {
+                                    pill: {
+                                        backgroundColor: theme.white,
+                                        color: theme.white,
+                                    },
+                                },
+                            })}/>
+                            <MultiSelect label="Indices" data={['Consultar', 'Exportar']} styles={(theme)=>({
+                                wrapper: {
+                                    pill: {
+                                        backgroundColor: theme.white,
+                                        color: theme.white,
+                                    },
+                                },
+                            })}/>
+                            <MultiSelect label="Reportes" data={['Consultar', 'Exportar']} styles={(theme)=>({
+                                wrapper: {
+                                    pill: {
+                                        backgroundColor: theme.white,
+                                        color: theme.white,
+                                    },
+                                },
+                            })}/>
+                            <MultiSelect label="Cédulas" data={['Consultar', 'Exportar']} styles={(theme)=>({
+                                wrapper: {
+                                    pill: {
+                                        backgroundColor: theme.white,
+                                        color: theme.white,
+                                    },
+                                },
+                            })}/>
                         </Flex>
                     </Flex>
                     <Group position='center' align="center" mt={16}>
@@ -46,7 +78,7 @@ function ModalEditarUsuario ({opened, close, info}) {
 
 ModalEditarUsuario.propTypes = {
     opened: PropTypes.bool,
-    info: PropTypes.object,
+    info: PropTypes.array,
     close: PropTypes.func,
 };
 
