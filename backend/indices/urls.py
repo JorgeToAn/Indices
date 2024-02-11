bcbf11d66ecd8a127ffc1a4cc751982d32ad325f
Julian Lopez <a20490712@itmexicali.edu.mx>
2024-02-11 07:17:34 -0800

    Se cambio float por decimal y redondeo a 2 numeros

--- backend/indices/urls.py
+++ backend/indices/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('permanencia/', views.IndicesPermanencia.as_view(), name='permanencia'),
     path('egreso/', views.IndicesEgreso.as_view(), name='egreso'),
     path('titulacion/', views.IndicesTitulacion.as_view(), name='titulacion'),
+    path('desercion/', views.IndicesDesercion.as_view(), name='desercion'),
 ]
--- backend/indices/views.py
+++ backend/indices/views.py
@@ -8,6 +8,8 @@ from rest_framework import permissions
 from registros.models import Ingreso, Egreso, Titulacion
 from registros.periodos import calcularPeriodos
 
+from decimal import Decimal
+
 class IndicesPermanencia(APIView):
     """
     Vista para listar la cantidad de alumnos por carrera.
@@ -53,7 +55,8 @@ class IndicesPermanencia(APIView):
             inactivos = Count("alumno__plan__carrera__pk", filter=Q(alumno_id__in=alumnos))
             poblacion_egr = Egreso.objects.aggregate(egresados=inactivos)
             poblacion_titulo = Titulacion.objects.aggregate(titulados=inactivos)
-            tasa_permanencia = (poblacion_act['poblacion']*100)/poblacion_nuevo_ingreso
+            tasa_permanencia = Decimal((poblacion_act['poblacion']*100)/poblacion_nuevo_ingreso)
+            tasa_permanencia = round(tasa_permanencia, 2)
             desercion = alumnos_corte_anterior - poblacion_act['poblacion'] - poblacion_egr['egresados']
             if desercion < 0:
                 desercion = 0
@@ -108,7 +111,8 @@ class IndicesEgreso(APIView):
             inactivos = Count("alumno__plan__carrera__pk", filter=Q(alumno_id__in=alumnos))
             poblacion_egr = Egreso.objects.aggregate(egresados=inactivos)
             poblacion_titulo = Titulacion.objects.aggregate(titulados=inactivos)
-            tasa_egreso = (poblacion_egr['egresados']*100)/poblacion_nuevo_ingreso
+            tasa_egreso = Decimal((poblacion_egr['egresados']*100)/poblacion_nuevo_ingreso)
+            tasa_egreso = round(tasa_egreso, 2)
             response_data[periodo] = dict(poblacion=poblacion_act['poblacion'], egresados=poblacion_egr['egresados'], titulados=poblacion_titulo['titulados'], tasa_egreso=tasa_egreso)
         return Response(response_data)
 
@@ -155,7 +159,67 @@ class IndicesTitulacion(APIView):
             inactivos = Count("alumno__plan__carrera__pk", filter=Q(alumno_id__in=alumnos))
             poblacion_egr = Egreso.objects.aggregate(egresados=inactivos)
             poblacion_titulo = Titulacion.objects.aggregate(titulados=inactivos)
-            tasa_titulacion = (poblacion_titulo['titulados']*100)/poblacion_nuevo_ingreso
+            tasa_titulacion = Decimal((poblacion_titulo['titulados']*100)/poblacion_nuevo_ingreso)
+            tasa_titulacion = round(tasa_titulacion, 2)
             response_data[periodo] = dict(poblacion=poblacion_act['poblacion'], egresados=poblacion_egr['egresados'], titulados=poblacion_titulo['titulados'], tasa_titulacion=tasa_titulacion)
+
+        return Response(response_data)
+
+class IndicesDesercion(APIView):
+    """
+    Vista para listar la cantidad de alumnos por carrera.
+
+    * Requiere autenticación por token.
+
+    ** nuevo-ingreso: Alumnos ingresando en 1er por examen o convalidacion
+    ** traslado-equivalencia: Alumnos ingresando de otro TEC u otra escuela
+    ** cohorte: El periodo donde empezara el calculo
+    ** semestres: Cuantos semestres seran calculados desde el cohorte
+    ** carrera: El programa educativo que se esta midiendo
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        nuevo_ingreso = request.GET.get('nuevo-ingreso')
+        traslado_equivalencia = request.GET.get('traslado-equivalencia')
+        cohorte = request.GET.get('cohorte') if request.GET.get('cohorte') else '20241'
+        semestres = request.GET.get('semestres') if request.GET.get('semestres') else '9'
+        carrera = request.GET.get('carrera')
+
+        tipos = []
+        if nuevo_ingreso:
+            tipos.extend(['EX', 'CO'])
+        if traslado_equivalencia:
+            tipos.extend(['TR', 'EQ'])
+
+        response_data = {}
+        periodos = calcularPeriodos(cohorte, int(semestres))
+        poblacion_nuevo_ingreso = 0
+        desercion_total = 0
+        alumnos = Ingreso.objects.filter(tipo__in=tipos, periodo=cohorte,alumno__plan__carrera__pk=carrera).annotate(clave=F("alumno_id")
+            ).values("clave")
+        alumnos_corte_anterior = 0
+        for periodo in periodos:
+            if periodo == cohorte:
+                activos = Count("alumno__plan__carrera__pk", filter=Q(tipo__in=tipos, periodo=cohorte,alumno__plan__carrera__pk=carrera))
+                poblacion_act = Ingreso.objects.aggregate(poblacion=activos)
+                poblacion_nuevo_ingreso = poblacion_act['poblacion']
+                alumnos_corte_anterior = poblacion_act['poblacion']
+            else:
+                activos = Count("alumno__plan__carrera__pk", filter=Q(tipo='RE', periodo=periodo, alumno__plan__carrera__pk=carrera))
+                poblacion_act = Ingreso.objects.aggregate(poblacion=activos)
+            inactivos = Count("alumno__plan__carrera__pk", filter=Q(alumno_id__in=alumnos))
+            poblacion_egr = Egreso.objects.aggregate(egresados=inactivos)
+            desercion = alumnos_corte_anterior - poblacion_act['poblacion'] - poblacion_egr['egresados']
+            if desercion < 0:
+                desercion = 0
+            desercion_total += desercion
+            tasa_desercion = Decimal((desercion_total*100)/poblacion_nuevo_ingreso)
+            tasa_desercion = round(tasa_desercion, 2)
+            alumnos_corte_anterior = poblacion_act['poblacion']
+            response_data[periodo] = dict(poblacion=poblacion_act['poblacion'], egresados=poblacion_egr['egresados'], desercion=desercion, tasa_desercion=tasa_desercion)
+            # filter(tipo__in=tipos, periodo=periodo, alumno__plan__carrera__pk=carrera).annotate(
+            #     clave=F("alumno__plan__carrera__pk")
+            #     ).values("clave").
 
         return Response(response_data)
