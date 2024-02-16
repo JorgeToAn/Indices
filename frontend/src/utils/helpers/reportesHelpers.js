d86a0a0619398468a66ae8c9ddfa95e83429bea0
julian lopez <a20490712@itmexicali.edu.mx>
2024-02-16 11:46:46 -0800

    Se conecto reportes nuevo ingreso en frontend

---
+++ frontend/src/utils/helpers/reportesHelpers.js
@@ -0,0 +1,24 @@
+import API from "../api";
+
+export const getReportesNuevoIngreso = async(nuevoIngreso, trasladoEquiv, cohorte, numSemestres) => {
+    const res =  await API.get('/reportes/nuevo-ingreso',  {
+        params: {'nuevo-ingreso':nuevoIngreso, 'traslado-equivalencia':trasladoEquiv, 'cohorte': cohorte.replace('-',''), 'semestres': numSemestres.toString()}
+    });
+    const reporte = buildTablaReportesNuevoIngreso(res.data);
+    return reporte;
+};
+
+const buildTablaReportesNuevoIngreso = (data) => {
+    const datos= Object.entries(data);
+    const tabla = [];
+    datos.forEach((registros) => {
+        const row = [registros[0]];
+        const regs = Object.entries(registros[1]);
+        regs.forEach((reg) => {
+            row.push(reg[1]['poblacion']);
+        });
+        tabla.push(row);
+    });
+    console.log(tabla);
+    return tabla;
+};
