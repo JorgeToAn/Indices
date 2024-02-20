752d5b29a2cac57dbc0df1b78a1da5d495db150c
julian lopez <a20490712@itmexicali.edu.mx>
2024-02-20 11:07:04 -0800

    Se agrego endpoint para tablas de crecimiento

--- backend/tablas/urls.py
+++ backend/tablas/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('poblacion/', views.TablasPoblacion.as_view(), name='poblacion'),
+    path('crecimiento/', views.TablasCrecimiento.as_view(), name='crecimiento'),
 ]
