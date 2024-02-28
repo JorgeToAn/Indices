b84c9fbeff7edb616e1272aced6b09d6d207230f
Julian Lopez <a20490712@itmexicali.edu.mx>
2024-02-27 20:45:53 -0800

    Inicio

--- frontend/src/utils/helpers/adminHelpers.js
+++ frontend/src/utils/helpers/adminHelpers.js
@@ -4,3 +4,7 @@ export const getListaUsuarios = async() => {
     const listaUsuarios = await API.get('usuario/lista/');
     return listaUsuarios.data;
 };
+
+export const cambiarContrasena = async() => {
+    const res = await API.get('usuario/cambiar/
+};
