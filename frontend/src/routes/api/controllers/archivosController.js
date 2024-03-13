40d16996652f2bfee89bc77d2862b1cbb5522073
julian lopez <a20490712@itmexicali.edu.mx>
2024-03-13 10:11:08 -0700

    Avances iniciales

---
+++ frontend/src/routes/api/controllers/archivosController.js
@@ -0,0 +1,13 @@
+import API from "../../../utils/api";
+
+export const subirIngresosExcel = async (archivo) => {
+    const f = archivo.get('file');
+    console.log(f);
+    const res = await API.post('registros/ingresos/subir/', archivo, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Content-Disposition': `attachment; filename=${f.name}`
+        }});
+    console.log(res);
+    return res;
+};
--- frontend/src/views/SubirArchivos.jsx
+++ frontend/src/views/SubirArchivos.jsx
@@ -12,27 +12,38 @@ import { Dropzone } from '@mantine/dropzone';
 import { ArrowLeft, FileUpload, Upload } from "tabler-icons-react";
 import { useDisclosure } from '@mantine/hooks';
 import ResultadosLog from '../components/modals/resultadosLog';
+import { subirIngresosExcel } from '../routes/api/controllers/archivosController';
+import { useState } from 'react';
 
 const SubirArchivos = () => {
     const [opened, { open, close }] = useDisclosure(false);
+    const [ingresos, setIngresos] = useState(null);
     const info = {
         "errores": ["Z20490712 no es un numero de control valido", "87907 no es un numero de control de valido"],
         "advertencias": ["Ya existe un registro de titulacion para el numero de control 20490213", "Ya existe un registro de titulacion para el numero de control 18490658"],
         "guardados": "Se guardaron 845 registros correctamente"
+    };
+    const subirIngresos = async () => {
+        const formData = new FormData();
+        formData.append('file', ingresos[0]);
+        console.log(ingresos);
+        const res = await subirIngresosExcel(formData);
+        console.log(res);
     };
     return (
         <Container>
             <ActionIcon color='naranja' variant='filled' radius='lg' mt={16} mb={16}>
                 <ArrowLeft />
             </ActionIcon>
-            {/* <Button color="naranja">
-                <ArrowLeft />
-            </Button> */}
             <Title order={3}>Subir Archivos</Title>
             <p>Aqui se suben los tres archivos que utiliza el sistema para trabajar, estos deberan ser cargados una vez por semestre.<br />Los archivos deben de seguir el siguiente formato para ser aceptados en el sistema.</p>
             <Group position='center'>
                 {/* Alumnos inscritos en el semestre */}
-                <Dropzone accept="MS_EXCEL_MIME_TYPE" onDrop={open}>
+                <Dropzone accept="MS_EXCEL_MIME_TYPE" onDrop={(file) => {
+                    setIngresos(file);
+                    subirIngresos();
+                }
+                }>
                     <Flex align="center" direction="column" position="center" gap="xl">
                         <Text fw={700} tt="capitalize">Alumnos Inscritos</Text>
                         {/* Es un icono */}
