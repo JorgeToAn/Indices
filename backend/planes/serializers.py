6289ec33f0ed521724360713783018ee9604c3e3
JorgeToAn <jorgetosta.f@gmail.com>
2024-04-04 17:59:50 -0700

    Actualizar serializers para Planes

--- backend/planes/serializers.py
+++ backend/planes/serializers.py
@@ -2,13 +2,28 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import Plan
 
-class PlanSerializer(serializers.ModelSerializer):
+class PlanGETSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
+        depth = 1
+
+    def validate(self, data):
+        """
+        Validar que la fecha final sea despues de la fecha de inicio
+        """
+        if data['fecha_final'] is not None:
+            if data['fecha_inicio'] >= data['fecha_final']:
+                raise serializers.ValidationError('La fecha final debe ser después de la fecha de inicio')
+        return data
+
+
+class PlanPOSTSerializer(serializers.ModelSerializer):
     clave = serializers.CharField(max_length=150, validators=[UniqueValidator(queryset=Plan.objects.all(), message='La clave debe de ser única.', lookup='iexact')])
 
     class Meta:
         model = Plan
         fields = '__all__'
-        depth = 1
 
     def validate(self, data):
         """
--- backend/planes/urls.py
+++ backend/planes/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PlanList.as_view(), name='plan-list'),
-    path('<int:pk>/', views.PlanDetail.as_view(), name='plan-detail'),
-    path('carreras/', views.CarreraPlanList.as_view(), name='carrera-plan-list'),
+    path('<str:pk>/', views.PlanDetail.as_view(), name='plan-detail'),
 
 ]
