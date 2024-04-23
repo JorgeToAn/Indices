d7fadad515422d5df59fe1e35f0d4e7aa797150e
julian lopez <a20490712@itmexicali.edu.mx>
2024-04-23 11:26:59 -0700

    Se movieron un poco las posiciones de los logos en el reporte de lista de alumnos en PDF

--- frontend/src/utils/helpers/export/pdfHelpers.js
+++ frontend/src/utils/helpers/export/pdfHelpers.js
@@ -28,23 +28,44 @@ export async function generatePDF(titulo, cohorte, numSemestres, heading, conten
     tecLogo.src = "/img/logo/Logo-TecNM.png";
     eduLogo.src = "/img/logo/sep_logo.png";
     itmLogo.src = "/img/logo/itmlogo.png";
-    doc.addImage(eduLogo, 'PNG', 50, 50, 160, 45);
-    doc.setDrawColor(179, 142,93);
-    doc.line(220, 60, 220, 90, 'S');
-    doc.addImage(tecLogo, 'PNG', 230, 50, 100, 45);
-    doc.line(340, 60, 340, 90, 'S');
-    doc.addImage(itmLogo, 'PNG', 350, 57, 21.6, 36);
-    doc.setDrawColor(0, 0,0);
-    doc.text('Instituto Tecnológico de Mexicali', 297.5, 130, null, null, 'center');
-    doc.line(60, 140, 550, 140);
-    doc.text(`Reporte de ${titulo} a ${numSemestres} semestres`, 297.5, 165, null, null,  'center');
-    doc.text(`Alumnos de ingreso por ${filtroAlumnos}`, 297.5, 185, null, null,  'center');
-    if(carrera !== ""){
-        const nombreC = await getNombreCarrera(carrera);
-        doc.text(nombreC, 297.5, 205, null, null,  'center');
-        doc.text(`Cohorte generacional ${cohorte}`, 297.5, 225, null, null,  'center');
+    if (multiPagina) {
+        doc.addImage(eduLogo, 'PNG', 200, 50, 160, 45);
+        doc.setDrawColor(179, 142,93);
+        doc.line(370, 60, 370, 90, 'S');
+        doc.addImage(tecLogo, 'PNG', 380, 50, 100, 45);
+        doc.line(490, 60, 490, 90, 'S');
+        doc.addImage(itmLogo, 'PNG', 500, 57, 21.6, 36);
+        doc.setDrawColor(0, 0,0);
+        doc.text('Instituto Tecnológico de Mexicali', 397.5, 130, null, null, 'center');
+        doc.line(160, 140, 650, 140);
+        doc.text(`Reporte de ${titulo} a ${numSemestres} semestres`, 397.5, 165, null, null,  'center');
+        doc.text(`Alumnos de ingreso por ${filtroAlumnos}`, 397.5, 185, null, null,  'center');
+        if(carrera !== ""){
+            const nombreC = await getNombreCarrera(carrera);
+            doc.text(nombreC, 397.5, 205, null, null,  'center');
+            doc.text(`Cohorte generacional ${cohorte}`, 397.5, 225, null, null,  'center');
+        } else {
+            doc.text(`Cohorte generacional ${cohorte}`, 397.5, 205, null, null,  'center');
+        }
     } else {
-        doc.text(`Cohorte generacional ${cohorte}`, 297.5, 205, null, null,  'center');
+        doc.addImage(eduLogo, 'PNG', 50, 50, 160, 45);
+        doc.setDrawColor(179, 142,93);
+        doc.line(220, 60, 220, 90, 'S');
+        doc.addImage(tecLogo, 'PNG', 230, 50, 100, 45);
+        doc.line(340, 60, 340, 90, 'S');
+        doc.addImage(itmLogo, 'PNG', 350, 57, 21.6, 36);
+        doc.setDrawColor(0, 0,0);
+        doc.text('Instituto Tecnológico de Mexicali', 297.5, 130, null, null, 'center');
+        doc.line(60, 140, 550, 140);
+        doc.text(`Reporte de ${titulo} a ${numSemestres} semestres`, 297.5, 165, null, null,  'center');
+        doc.text(`Alumnos de ingreso por ${filtroAlumnos}`, 297.5, 185, null, null,  'center');
+        if(carrera !== ""){
+            const nombreC = await getNombreCarrera(carrera);
+            doc.text(nombreC, 297.5, 205, null, null,  'center');
+            doc.text(`Cohorte generacional ${cohorte}`, 297.5, 225, null, null,  'center');
+        } else {
+            doc.text(`Cohorte generacional ${cohorte}`, 297.5, 205, null, null,  'center');
+        }
     }
     let  header = heading;
     if (!Array.isArray(heading[1])){
@@ -69,18 +90,21 @@ export async function generatePDF(titulo, cohorte, numSemestres, heading, conten
             head:  header,
             body: content.filter((row, index) => index < 17),
         });
-        doc.addPage();
-        autoTable(doc,{
-            headStyles: {
-            fillColor: '#1B396A',
-            fontSize: numSemestres > 10 ? 5 : 7,
-            },
-            bodyStyles: {
+        if (content.length > 16) {
+            doc.addPage();
+            autoTable(doc,{
+                headStyles: {
+                fillColor: '#1B396A',
                 fontSize: numSemestres > 10 ? 5 : 7,
-            },
-            margin: { top: 50 },
-            head:  header,
-            body: content.filter((row, index) => index >= 17),});
+                },
+                bodyStyles: {
+                    fontSize: numSemestres > 10 ? 5 : 7,
+                },
+                margin: { top: 50 },
+                head:  header,
+                body: content.filter((row, index) => index >= 17),
+            });
+        }
     } else {
         autoTable(doc, {
             headStyles: {
