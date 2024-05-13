f36ded4ec98b94a14fa47de58f54493a7fe1c368
Julian Lopez <a20490712@itmexicali.edu.mx>
2024-05-12 20:13:58 -0700

    Cambio de permisos en vista mi perfil

--- frontend/src/views/usuario/MiPerfil.jsx
+++ frontend/src/views/usuario/MiPerfil.jsx
@@ -7,50 +7,66 @@ import {
     Group,
     TextInput
 } from "@mantine/core";
-import { DeviceFloppy, Edit } from "tabler-icons-react";
+import { DeviceFloppy, Edit, ShieldCheck } from "tabler-icons-react";
 import Header from "src/components/header.jsx";
-import Permisos from "src/components/permisos.jsx";
 import { useAuthStore } from "src/store/auth";
+import { useEffect, useState } from "react";
+import dropDownData from "src/mockup/dropDownData";
+import { asignarPermiso, removerTodosPermisos } from "src/routes/api/controllers/permisoController";
+import { notifications } from "@mantine/notifications";
 
 
 const MiPerfil = () => {
     const user = useAuthStore((state) => state.user);
+    const [onEdit, setOnEdit] = useState(false);
+    const [carreras, setCarreras] = useState([]);
+    const [permisos, setPermisos] = useState([]);
+    const fetchCarreras = async() => {
+        const c = await dropDownData.getListaCarrerasAll();
+        const cA = await dropDownData.getListaCarrerasForUser(user().user_id);
+        setPermisos(cA.map((carrera) => carrera.value));
+        setCarreras(c);
+    };
 
-    // Realizar fetch de las carreras registradas
-    const carreras = [
-        "Ing. Sistemas Computacionales",
-        "Contador Público",
-        "Ing. Química",
-        "Ing. Energías Renovables",
-        "Ing. Mecánica",
-        "Ing. Mecatrónica",
-        "Ing. Eléctrica",
-        "Ing. Electrónica",
-        "Ing. Logística",
-        "Ing. Gestión Empresarial"
-    ];
+    const updateUser = async() => {
+        console.log(permisos);
+        await removerTodosPermisos(user().user_id);
+        permisos.forEach(async (clave) => {
+            await asignarPermiso(clave, user().user_id);
+        });
+        notifications.show({
+            message: `Se han cambiado los permisos del usuario "${user().username}" con éxito.`,
+            color: 'teal',
+            icon: <ShieldCheck size={20} />,
+          });
+    };
 
-    const wrapper_divs = document.getElementsByClassName("user-info");
+    useEffect(() => {
+        fetchCarreras();
+
+    }, []);
+
+    // const wrapper_divs = document.getElementsByClassName("user-info");
     const editarUsuario = () => {
-        console.log(user());
+        setOnEdit(true);
         document.getElementById("btns").style.display = "block";
         document.getElementById("btn-editar").style.display = "none";
-        for (const div in wrapper_divs) {
-            const input = wrapper_divs[div].children[1].children;
-            input[0].removeAttribute("disabled");
-            input[0].removeAttribute("data-disabled");
-        }
+        // for (const div in wrapper_divs) {
+        //     const input = wrapper_divs[div].children[1].children;
+        //     input[0].removeAttribute("disabled");
+        //     input[0].removeAttribute("data-disabled");
+        // }
     };
 
     const cancelarEdicion = () => {
         document.getElementById("btns").style.display = "none";
         document.getElementById("btn-editar").style.display = "block";
-
-        for (const div in wrapper_divs) {
-            const input = wrapper_divs[div].children[1].children;
-            input[0].setAttribute("disabled", true);
-            input[0].setAttribute("data-disabled", true);
-        }
+        setOnEdit(false);
+        // for (const div in wrapper_divs) {
+        //     const input = wrapper_divs[div].children[1].children;
+        //     input[0].setAttribute("disabled", true);
+        //     input[0].setAttribute("data-disabled", true);
+        // }
     };
 
     return (
@@ -62,28 +78,36 @@ const MiPerfil = () => {
             <Group align="flex-start" mt={20} spacing="xl" >
                 <Flex direction="column">
                     <form>
-                        <TextInput id="username" className="user-info" value={user().username} disabled withAsterisk label="Nombre de usuario"/>
-                        <TextInput id="email" className="user-info"  value={user().email} disabled withAsterisk label="Correo electrónico"/>
-                        <TextInput id="first_name" className="user-info" value={user().first_name} disabled withAsterisk label="Nombre"/>
-                        <TextInput id="paternal_surname" className="user-info" value={user().paternal_surname} disabled withAsterisk label="Apellido Paterno"/>
-                        <TextInput id="maternal_surname" className="user-info" value={user().maternal_surname} disabled  label="Apellido Materno"/>
-                        <TextInput id="gender" className="user-info" value={user().gender} disabled  label="Sexo"/>
+                        <TextInput disabled={!onEdit} id="username" className="user-info" value={user().username} withAsterisk label="Nombre de usuario"/>
+                        <TextInput id="email" className="user-info"  value={user().email} disabled={!onEdit} withAsterisk label="Correo electrónico"/>
+                        <TextInput id="first_name" className="user-info" value={user().first_name} disabled={!onEdit} withAsterisk label="Nombre"/>
+                        <TextInput id="paternal_surname" className="user-info" value={user().paternal_surname} disabled={!onEdit} withAsterisk label="Apellido Paterno"/>
+                        <TextInput id="maternal_surname" className="user-info" value={user().maternal_surname} disabled={!onEdit}  label="Apellido Materno"/>
+                        <TextInput id="gender" className="user-info" value={user().gender} disabled={!onEdit}  label="Sexo"/>
                         <Button leftIcon={<Edit />}  mt={16} id="btn-editar" w="100%" onClick={editarUsuario}>Editar</Button>
                         <Flex direction="column" id="btns" wrap="wrap" justify="center" align="center" style={{display:"none"}}>
-                            <Button leftIcon={<DeviceFloppy />} w="100%" id="btn-actualizar" mt={16} type="submit">Actualizar</Button>
+                            <Button leftIcon={<DeviceFloppy />} w="100%" id="btn-actualizar" mt={16} onClick={updateUser}>Actualizar</Button>
                             <Button mt={16} color="gris" w="100%" onClick={cancelarEdicion} id="btn-cancelar">Cancelar</Button>
                         </Flex>
                     </form>
                 </Flex>
                 <Flex direction="column" align="center" justify="flex-start" >
-                    <Permisos />
                     <Accordion variant="contained" radius="md" mt={16}>
                         <Accordion.Item value="carreras" w="300px" >
                             <Accordion.Control><b>Carreras</b></Accordion.Control>
                             <Accordion.Panel>
                                 <List withPadding listStyleType="none">
-                                    { carreras.map((carrera,index) =><List.Item key={index}><Checkbox label={carrera} labelPosition="right" radius="sm" /></List.Item> )}
-                                </List>
+                                    { carreras.map((carrera,index) =><List.Item key={index}><Checkbox disabled={!onEdit} checked={permisos.filter((c) => c === carrera.value).length > 0} label={carrera.label} labelPosition="right" radius="sm" onChange={(e) => {
+                                        let copyPermisos = [...permisos];
+                                        if (e.target.checked) {
+                                            copyPermisos.push(carrera.value);
+                                            setPermisos(copyPermisos);
+                                        } else {
+                                            copyPermisos = copyPermisos.filter((clave) => clave !== carrera.value);
+                                            setPermisos(copyPermisos);
+                                        }
+                                    }} /></List.Item> )}
+                                    </List>
                             </Accordion.Panel>
                         </Accordion.Item>
                     </Accordion>
